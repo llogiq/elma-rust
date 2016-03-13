7bdf1d9a3da885ba1fe358347665b868e97a0aca
Roger Andersen <hexjelly@hexjelly.com>
2016-03-13 18:52:33 +0100

    Add more random stuff and hope it works out

--- src/lev/mod.rs
+++ src/lev/mod.rs
@@ -43,20 +43,33 @@ pub struct Picture {
 
 /// Level struct that contains all level information
 pub struct Level {
+    /// Raw binary data of a loaded or finalized constructed level
+    pub raw: Vec<u8>,
+    /// Random number that links level file to replay files
     pub link: u32,
+    /// Contains four integrity checks (See create_integrity())
     pub integrity: [f64; 4],
+    /// Level name
     pub name: [u8; 51],
+    /// LGR file name
     pub lgr: [u8; 16],
+    /// Ground texture name
     pub ground: [u8; 10],
+    /// Sky texture name
     pub sky: [u8; 10],
+    /// Vector with all polygons (See Polygon)
     pub polygons: Vec<Polygon>,
+    /// Vector with all objects (See Object)
     pub objects: Vec<Object>,
+    /// Vector with all pictures (See Picture)
     pub pictures: Vec<Picture>
 }
 
 impl Level {
+    /// Build a new Level
     pub fn new () -> Level {
         Level {
+            raw: Vec::new(),
             link: 0,
             integrity: [0.0f64; 4],
             name: [0; 51],
@@ -69,17 +82,21 @@ impl Level {
         }
     }
 
-    fn parse_level (&self, buffer: Vec<u8>) {
+    /// Parses raw binary level data from self.raw and populates Level fields with information
+    pub fn parse_level (&self) {
 
     }
 
-    pub fn load_level (&self, filename: &str) {
+    /// Loads level file data into self.raw field then calls self.parse_level()
+    pub fn load_level (&mut self, filename: &str) {
         let mut file = File::open(filename).unwrap();
         let mut buffer = Vec::new();
-        file.read_to_end(&mut buffer);
-        self.parse_level(buffer);
+        file.read_to_end(&mut buffer).unwrap();
+        self.raw = buffer;
+        self.parse_level();
     }
 
+    /// Saves self.raw data to a file
     pub fn save_lev (&self, filename: &str) {
         let mut file = File::create(&filename).unwrap();
         file.write_all(b"Hello, world!").unwrap();
